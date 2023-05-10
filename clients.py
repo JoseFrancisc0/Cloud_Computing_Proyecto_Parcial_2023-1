74a134954b8e08f736b0c9bfcf6ec31131e8d064
JoseFrancisc0 <jose.wong@utec.edu.pe>
2023-05-10 01:11:31 -0500

    Client API Microservice

--- clients.py
+++ clients.py
@@ -1,40 +1,65 @@
-from flask import Blueprint, jsonify, request
-from app import db
-from models import Client
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
-# BP de la API
-clients_api = Blueprint('clients', __name__)
+# Flask/SQLAlchemy instance
+clients_api = Flask(__name__)
+clients_api.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:nKaqQ1wlHFBq3cGCvB6u@database-proyecto.crt5dlbdpqks.us-east-1.rds.amazonaws.com:5432/postgres"
+clients_api.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(clients_api)
 
-# CREATE
+# Client Model
+class Client(db.Model):
+    __tablename__ = 'clients'
+    id = db.Column(db.Integer, primary_key = True, nullable = False)
+    firstname = db.Column(db.String(30), nullable = False)
+    lastname = db.Column(db.String(30), nullable = False)
+    reservation = db.relationship("Reservation", backref="clients")
+
+    def __repr__(self):
+        return f'<Client {self.id}>'
+
+# 404 Error Handler 
+@clients_api.errorhandler(404)
+def not_found(error):
+    return jsonify({'error' : 'Not found.'}), 404
+
+# 500 Error Handler
+@clients_api.errorhandler(500)
+def internal_server_error(error):
+    return jsonify({'error' : 'Internal server error.'}), 500
+
+# CREATE API Endopoint
 @clients_api.route('/clients', methods = ['POST'])
 def create_client():
     data = request.get_json()
     client = Client(firstname = data['firstname'], lastname = data['lastname'])
     db.session.add(client)
     db.session.commit()
-    return jsonify(client.serialize()), 201
+    return jsonify({'message' : 'Client created successfully'}), 201
 
-# READ (all)
+# READ (all) API Endpoint
 @clients_api.route('/clients', methods = ['GET'])
 def get_clients():
     clients = Client.query.all()
-    return jsonify([client.serialize() for client in clients]), 200
+    return jsonify([{'id': client.id,
+                     'firstname': client.firstname,
+                     'lastname': client.lastname} for client in clients]), 200
 
-# READ (each)
+# READ (each) API Endpoint
 @clients_api.route('/clients/<int:id>', methods = ['GET'])
 def get_client(id):
     client = Client.query.get(id)
     if client is None:
-        return jsonify({'error': 'Client not found'}), 404
+        return not_found(404)
     return jsonify(client.serialize()), 200
 
-# UPDATE
+# UPDATE API Endpoint
 @clients_api.route('/clients/<int:id>', methods = ['PATCH'])
 def update_client(id):
     client = Client.query.get(id)
 
     if client is None:
-        return jsonify({'error' : 'Client not found'}), 404
+        return not_found(404)
 
     data = request.get_json()
     if 'firstname' in data:
@@ -42,15 +67,19 @@ def update_client(id):
     if 'lastname' in data:
         client.lastname = data['lastname']
     db.session.commit()
-    return jsonify(client.serialize()), 200
+    return jsonify({'message' : 'Client updated successfully'}), 200
 
-# DELETE
+# DELETE API Endpoint
 @clients_api.route('/clients/<int:id>', methods = ['DELETE'])
 def delete_client(id):
     client = Client.query.get(id)
     if client is None:
-        return jsonify({'error' : 'Client not found'}), 404
+        return not_found(404)
 
     db.session.delete(client)
     db.session.commit()
-    return '',204
+    return jsonify({'message' : 'Client deleted successfully'}), 204
+
+# Run
+if __name__ == '__main__':
+    clients_api.run(port = 8011, debug = True)
