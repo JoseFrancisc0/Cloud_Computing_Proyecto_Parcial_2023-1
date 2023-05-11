102aa0d49a2d2e24f9e5112927d5bd889e027cd2
JoseFrancisc0 <jose.wong@utec.edu.pe>
2023-05-11 17:14:12 -0500

    Added CORS functionality to cars API and added image column

--- Backend Microservices/cars/cars.py
+++ Backend Microservices/cars/cars.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 # Flask/SQLAlchemy instance
 cars_api = Flask(__name__)
 cars_api.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:nKaqQ1wlHFBq3cGCvB6u@database-proyecto.crt5dlbdpqks.us-east-1.rds.amazonaws.com:5432/postgres"
 cars_api.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(cars_api)
+CORS(cars_api)
 
 # Car Model
 class Car(db.Model):
@@ -16,6 +18,7 @@ class Car(db.Model):
     type_of_car = db.Column(db.String(30), nullable = False)
     year_car = db.Column(db.Integer, nullable = False)
     cost_per_day = db.Column(db.Integer, nullable = False)
+    image = db.Column(db.String(150), nullable = False)
     def __repr__(self):
         return f'<Car {self.id}>'
 
@@ -33,7 +36,7 @@ def internal_server_error(error):
 @cars_api.route('/cars', methods = ['POST'])
 def create_car():
     data = request.get_json()
-    car = Car(brand = data['brand'], model = data['model'], type_of_car = data['type_of_car'], year_car = data['year_car'], cost_per_day = data['cost_per_day'])
+    car = Car(brand = data['brand'], model = data['model'], type_of_car = data['type_of_car'], year_car = data['year_car'], cost_per_day = data['cost_per_day'], image = data['image'])
     db.session.add(car)
     db.session.commit()
     return jsonify({'message' : 'Car created successfully'}), 201
@@ -47,7 +50,8 @@ def get_cars():
                         'model': car.model,
                         'type_of_car' : car.type_of_car,
                         'year_car' : car.year_car,
-                        'cost_per_day' : car.cost_per_day} for car in cars]), 200
+                        'cost_per_day' : car.cost_per_day,
+                        'image': car.image} for car in cars]), 200
 
 # READ (each)
 @cars_api.route('/cars/<int:id>', methods = ['GET'])
@@ -60,7 +64,8 @@ def get_car(id):
                         'model': car.model,
                         'type_of_car' : car.type_of_car,
                         'year_car' : car.year_car,
-                        'cost_per_day' : car.cost_per_day}), 200
+                        'cost_per_day' : car.cost_per_day,
+                        'image': car.image}), 200
 
 # UPDATE
 @cars_api.route('/cars/<int:id>', methods = ['PATCH'])
@@ -81,6 +86,8 @@ def update_car(id):
         car.year_car = data['year_car']
     if 'cost_per_day' in data:
         car.cost_per_day = data['cost_per_day']
+    if 'image' in data:
+        car.image = data['image']
 
     db.session.commit()
     return jsonify({'message' : 'Car updated successfully'}), 200
